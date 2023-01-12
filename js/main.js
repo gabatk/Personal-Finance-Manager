a5024f35564d14afd459122f9aa6eccdc3538f7e
gabatk <gabriela.trybuszek@gmail.com>
2023-01-12 22:40:05 +0100

    App style change function completed.

--- js/main.js
+++ js/main.js
@@ -32,5 +32,19 @@ const doNotAdd = () => {
 	CategorySelect.selectedIndex = 0;
 };
 
+
+
 addTransactionBtn.addEventListener('click', addTransaction);
 cancelBtn.addEventListener('click', doNotAdd);
+
+lightStyle.addEventListener('click', () => {
+	root.style.setProperty('--first-color', '#f9f9f9');
+	root.style.setProperty('--second-color', '#14161f');
+	root.style.setProperty('--border-color', 'rgba(0, 0, 0, 0.2)');
+});
+
+darkStyle.addEventListener('click', () => {
+	root.style.setProperty('--first-color', '#14161f');
+	root.style.setProperty('--second-color', '#f9f9f9');
+	root.style.setProperty('--border-color', 'rgba(255, 255, 255, 0.2)');
+});
