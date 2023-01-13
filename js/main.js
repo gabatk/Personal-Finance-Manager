c7859be8a22d62bf700aeb2f99609452b83296dd
gabatk <gabriela.trybuszek@gmail.com>
2023-01-13 19:35:19 +0100

    Made some changes in main.js.

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ const expensesSection = document.querySelector('.expenses-area');
 const availableMoney = document.querySelector('.options__available-money');
 const addTransactionPanel = document.querySelector('.add-transaction__panel');
 
-const NameInput = document.querySelector('#name');
-const AmountInput = document.querySelector('#amount');
-const CategorySelect = document.querySelector('#category');
+const nameInput = document.querySelector('#name');
+const amountInput = document.querySelector('#amount');
+const categorySelect = document.querySelector('#category');
 
 const addTransactionBtn = document.querySelector('.add-transaction');
 const deleteAllBtn = document.querySelector('.delete-all');
@@ -27,24 +27,30 @@ const openTransactionPanel = () => {
 
 const closeTransactionPanel = () => {
 	addTransactionPanel.style.display = 'none';
-	NameInput.value = '';
-	AmountInput.value = '';
-	CategorySelect.selectedIndex = 0;
+	clearInputs();
 };
 
-const addTransaction = () => {
+const checkForm = () => {
 	if (
-		NameInput.value !== '' &&
-		AmountInput.value !== '' &&
-		CategorySelect.selectedIndex !== 0
+		nameInput.value !== '' &&
+		amountInput.value !== '' &&
+		categorySelect.value !== 'none'
 	) {
 		addTransactionPanel.style.display = 'none';
+	} else {
+		alert('You should fill in all the fields.');
 	}
+};
+
+const clearInputs = () => {
+	nameInput.value = '';
+	amountInput.value = '';
+	categorySelect.selectedIndex = 0;
 };
 
 addTransactionBtn.addEventListener('click', openTransactionPanel);
 cancelBtn.addEventListener('click', closeTransactionPanel);
-saveBtn.addEventListener('click', addTransaction);
+saveBtn.addEventListener('click', checkForm);
 
 lightStyle.addEventListener('click', () => {
 	root.style.setProperty('--first-color', '#f9f9f9');
